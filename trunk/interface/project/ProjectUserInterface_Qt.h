bf9e646e16fc201ceee7135f61052a923f5255ad
irvingbadolato <irvingbadolato@d8036de5-5264-49cf-ab17-72cb7252746c>
2010-09-22 19:40:32 +0000

    Atualizada a interface de imagem e ponto para evitar bugs.

--- trunk/interface/project/ProjectUserInterface_Qt.h
+++ trunk/interface/project/ProjectUserInterface_Qt.h
@@ -25,55 +25,55 @@
 class TreeItem
 {
 public:
-    TreeItem(const QList<QVariant> &data, TreeItem *parent = 0);
-    ~TreeItem();
+	TreeItem(const QList<QVariant> &data, TreeItem *parent = 0);
+	~TreeItem();
 
-    void appendChild(TreeItem *child);
+	void appendChild(TreeItem *child);
 
-    TreeItem *child(int row);
-    int childCount() const;
-    int columnCount() const;
-    QVariant data(int column) const;
-    int row() const;
-    TreeItem *parent();
+	TreeItem *child(int row);
+	int childCount() const;
+	int columnCount() const;
+	QVariant data(int column) const;
+	int row() const;
+	TreeItem *parent();
 
 private:
-    QList<TreeItem*> childItems;
-    QList<QVariant> itemData;
-    TreeItem *parentItem;
+	QList<TreeItem*> childItems;
+	QList<QVariant> itemData;
+	TreeItem *parentItem;
 };
 
 class TreeModel : public QAbstractItemModel
 {
-    Q_OBJECT
+	Q_OBJECT
 
 public:
-    TreeModel(const QString &data, QObject *parent = 0);
-    ~TreeModel();
+	TreeModel(const QString &data, QObject *parent = 0);
+	~TreeModel();
 
-    QVariant data(const QModelIndex &index, int role) const;
-    Qt::ItemFlags flags(const QModelIndex &index) const;
-    QVariant headerData(int section, Qt::Orientation orientation,
-                        int role = Qt::DisplayRole) const;
-    QModelIndex index(int row, int column,
-                      const QModelIndex &parent = QModelIndex()) const;
-    QModelIndex parent(const QModelIndex &index) const;
-    int rowCount(const QModelIndex &parent = QModelIndex()) const;
-    int columnCount(const QModelIndex &parent = QModelIndex()) const;
+	QVariant data(const QModelIndex &index, int role) const;
+	Qt::ItemFlags flags(const QModelIndex &index) const;
+	QVariant headerData(int section, Qt::Orientation orientation,
+						int role = Qt::DisplayRole) const;
+	QModelIndex index(int row, int column,
+					  const QModelIndex &parent = QModelIndex()) const;
+	QModelIndex parent(const QModelIndex &index) const;
+	int rowCount(const QModelIndex &parent = QModelIndex()) const;
+	int columnCount(const QModelIndex &parent = QModelIndex()) const;
 
 private:
-    void setupModelData(const QStringList &lines, TreeItem *parent);
+	void setupModelData(const QStringList &lines, TreeItem *parent);
 
-    TreeItem *rootItem;
+	TreeItem *rootItem;
 };
 
 class ProjectUserInterface_Qt : public QMainWindow, public Ui::ProjectMainWindow, public ProjectUserInterface
 {
-    Q_OBJECT
+	Q_OBJECT
 
 public:
-    ProjectUserInterface_Qt(ProjectManager* manager, QWidget* parent = 0, Qt::WindowFlags fl = Qt::Window);
-    ~ProjectUserInterface_Qt();
+	ProjectUserInterface_Qt(ProjectManager* manager, QWidget* parent = 0, Qt::WindowFlags fl = Qt::Window);
+	~ProjectUserInterface_Qt();
 
 protected:
 	TreeModel* treeModel;
@@ -95,22 +95,24 @@ protected:
 protected slots:
 	virtual void languageChange();
 	virtual void updateTree();
+	virtual void validatingImage();
+	virtual void validatingPoint();
 
 public:
 
-    // Other Methods
-    //
-    bool exec();
+	// Other Methods
+	//
+	bool exec();
 
-    void viewHeader();
-    void viewTerrain();
-    void viewFlights();
-    void viewFlight(int id);
-    void viewSensors();
-    void viewSensor(int id);
-    void viewPoints();
-    void viewPoint(int id);
-    void viewImages();
+	void viewHeader();
+	void viewTerrain();
+	void viewFlights();
+	void viewFlight(int id);
+	void viewSensors();
+	void viewSensor(int id);
+	void viewPoints();
+	void viewPoint(int id);
+	void viewImages();
 	void viewImage(int id);
 	//void updateView(AbstractForm& newView);
 
@@ -127,39 +129,39 @@ public slots:
 	virtual void selectImage(int);
 	virtual void selectPoint(int);
 	virtual void enableForm();
-    // Salvando cada view...
-    virtual void saveHeader();
-    virtual void saveTerrain();
-    virtual void saveFlight();
-    virtual void saveSensor();
-    virtual void savePoint();
-    virtual void saveImage();
-    // Cancelando cada view...
-    virtual void cancelHeader();
-    virtual void cancelTerrain();
-    virtual void cancelFlight();
-    virtual void cancelSensor();
-    virtual void cancelPoint();
-    virtual void cancelImage();
-    virtual void cancelFlights();
-    virtual void cancelSensors();
-    virtual void cancelPoints();
-    virtual void cancelImages();
-    // Criando um vazio de cada view...
-    virtual void newFlight();
-    virtual void newSensor();
-    virtual void newPoint();
-    virtual void newImage();
-    // Salvando um novo de cada view...
-    virtual void saveNewFlight();
-    virtual void saveNewSensor();
-    virtual void saveNewPoint();
-    virtual void saveNewImage();
-    // Apagando cada view...
-    virtual void deleteFlight();
-    virtual void deleteSensor();
-    virtual void deletePoint();
-    virtual void deleteImage();
+	// Salvando cada view...
+	virtual void saveHeader();
+	virtual void saveTerrain();
+	virtual void saveFlight();
+	virtual void saveSensor();
+	virtual void savePoint();
+	virtual void saveImage();
+	// Cancelando cada view...
+	virtual void cancelHeader();
+	virtual void cancelTerrain();
+	virtual void cancelFlight();
+	virtual void cancelSensor();
+	virtual void cancelPoint();
+	virtual void cancelImage();
+	virtual void cancelFlights();
+	virtual void cancelSensors();
+	virtual void cancelPoints();
+	virtual void cancelImages();
+	// Criando um vazio de cada view...
+	virtual void newFlight();
+	virtual void newSensor();
+	virtual void newPoint();
+	virtual void newImage();
+	// Salvando um novo de cada view...
+	virtual void saveNewFlight();
+	virtual void saveNewSensor();
+	virtual void saveNewPoint();
+	virtual void saveNewImage();
+	// Apagando cada view...
+	virtual void deleteFlight();
+	virtual void deleteSensor();
+	virtual void deletePoint();
+	virtual void deleteImage();
 	// Debug...
 	virtual void toggleDebug();
 };
