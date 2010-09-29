5308d2c0b76b2f3ef889fedb8fca51fc1a85f872
andreengineer <pandreengineer@gmail.com>
2010-09-29 21:37:03 +0000

    Adicionado o novo Formulario de Sensor

--- interface/project/SensorForm.h
+++ interface/project/SensorForm.h
@@ -1,51 +1,29 @@
-/***************************************************************************
- *   E-Foto Project: Digital Photogrammetric Workstation                   *
- *   Rio de Janeiro State University                                       *
- *   Program of Post-Graduation in Geomatics                               *
- *                                                                         *
- *   Copyright (C) 2010 by E-Foto Project                                  *
- *                                                                         *
- *   Developers:           Guilherme Lucio Abelha Mota                     *
- *                         Bruno Cardozo Cotrim da Costa                   *
- *                                                                         *
- *   contact:  guimota at ime.uerj.br                                      *
- *                                                                         *
- *   This program is free software; you can redistribute it and/or modify  *
- *   it under the terms of the GNU General Public License as published by  *
- *   the Free Software Foundation; either version 2 of the License, or     *
- *   (at your option) any later version.                                   *
- *                                                                         *
- *   This program is distributed in the hope that it will be useful,       *
- *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
- *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
- *   GNU General Public License for more details.                          *
- *                                                                         *
- *   You should have received a copy of the GNU General Public License     *
- *   along with this program; if not, write to the                         *
- *   Free Software Foundation, Inc.,                                       *
- *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
- ***************************************************************************/
 #ifndef SENSORFORM_H
 #define SENSORFORM_H
 
+#include "ui_SensorLayout.h"
 #include "AbstractForm.h"
-#include "sensorview.h"
-#include <string>
+#include "EDom.h"
 
-class SensorForm : public AbstractForm //, public Ui::Form
+class SensorForm : public AbstractForm, public Ui::SensorView
 {
     Q_OBJECT
 
 public:
-    SensorForm(QWidget *parent = 0, string XmlString="");
+    EDomElement ede;
+    SensorForm(QWidget *parent =0);
 
     void fillvalues(string values);
     string getvalues();
     void setReadOnly(bool state);
-	bool isForm(string formName);
-private:
+    bool isForm(string formName);
+    void showLast4Fiductial(bool state);
 
-    SensorView* psensorview;
+    public slots:
+            void detectorGroup(int);
+            void fiductialGroup(int);
+            void setSigmasVisible(bool);
+
 };
 
 #endif // SENSORFORM_H
