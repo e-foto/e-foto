43b1370c66b92833c943f9d0d9878476fb090b0f
andreengineer <andreengineer@d8036de5-5264-49cf-ab17-72cb7252746c>
2011-03-10 17:49:49 +0000

    Added a dir with a .pro file for test TableIOEOWidget Enhancements in DmsEdit

--- branches/SandBox/TableIOEOWidget/TableIOEOWidget.h
+++ branches/SandBox/TableIOEOWidget/TableIOEOWidget.h
@@ -4,18 +4,16 @@
 #include <QTableWidget>
 #include <QTableWidgetItem>
 #include <QResizeEvent>
-#include <QLabel>
-#include "EDom.h"
+
 #include "Matrix.h"
-
-
 class TableIOEOWidget: public QTableWidget
 {
     Q_OBJECT
 private:
     Matrix *io;
-	char mode;
-	int decimals;
+//Novo 11.03.08
+    char mode;
+    int decimals;
 
 public:
     TableIOEOWidget(QWidget *parent=0);
@@ -23,31 +21,26 @@ public:
     void setTableData(Matrix values,char mode='e',int precision=6);
     void setTableData(EDomElement xml,char mode='e',int precision=6);
     void setTableData(string xmlString,char mode='e',int precision=6);
-	void setVerticalHeadersLabelsHtml(QStringList list);
-	void resizeTable();
-    void enableAutoCopy(bool enable=true);
+    void resizeTable();
 
-	void setMode(char newMode);
-	void setDecimals(int newDecimals);
-	char getMode();
-	int getDecimals();
+    //Novo 11.03.08
+    void setMode(char newMode);
+    void setDecimals(int newDecimals);
+    char getMode();
+    int getDecimals();
 
 public slots:
     void autoCopy();
-	void enableAutoCopy(int enable);
-	void updateTableValues(char mode, int precision);
-	void updateModoValues(int modo);
-	void updateDecimalsValues(int decimal);
-
-signals:
-    void focusReceived();
-
+    void enableAutoCopy(bool enable=true);
+    //Novo 11.03.08
+    void enableAutoCopy(int enable);
+    void updateTableValues(char mode, int precision);
+    void updateModoValues(int modo);
+    void updateDecimalsValues(int decimal);
 protected:
    // void resizeEvent(QResizeEvent *event);
     void keyPressEvent(QKeyEvent *event);
-    bool eventFilter(QObject *, QEvent *);
-    void focusInEvent(QFocusEvent *event);
-    //void dragMoveEvent(QDragMoveEvent *event);
+    void resizeEvent(QResizeEvent *event);
 };
 
 #endif // TABLEIOEOWIDGET_H
