3eaf83931e19237410debc553183ae5c320324a1
andreengineer <pandreengineer@gmail.com>
2012-03-15 16:46:21 +0000

    Documented ETableWidget , FlightDirectionForm and EfotoDoubleSpinBox classes

--- trunk_candidata/qt/infrastructure/ETableWidget.h
+++ trunk_candidata/qt/infrastructure/ETableWidget.h
@@ -15,6 +15,14 @@ namespace uerj {
 namespace eng {
 namespace efoto {
 
+/**
+* \file ETableWidget.h
+* \class ETableWidget
+* \brief Classe que gerencia as tabelas reimplementada da QTableWidget
+* \copyright E-Foto group
+* \author Paulo André Batista Pupim e Irving Badolato
+*/
+
 class ETableWidget: public QTableWidget
 {
 	Q_OBJECT
@@ -33,59 +41,187 @@ private:
 	QList<int> typedDoubleSpinColumns;
 
 public:
+	/**
+	* \brief Construtor default
+	* \param parent: Ponteiro para objeto QWidget pai
+	*/
 	ETableWidget(QWidget *parent=0);
+
+	/**
+	* \brief Construtor com valores numéricos e alguma formatação para exibição da tabela
+	* \param values: Matriz contendo os valores numéricos a serem exibidos na tabela
+	* \param mode  : Tipo de exibição com que os valores numéricos aparecerão: 'e' é formato exponencial , 'f' é formato decimal
+	* \param precision: Precisão com a qual os valores numericos serão exibidos
+	* \param parent: Ponteiro para objeto QWidget pai
+	*/
 	ETableWidget(Matrix values,char mode='e',int precision=6,QWidget *parent=0);
+
+	/**
+	* \brief Metodo para inserir valores numericos na tabela a partir da coluna zero e linha zero
+	* \param values: Matriz contendo os valores numéricos a serem exibidos na tabela
+	* \param mode  : Tipo de exibição com que os valores numéricos aparecerão: 'e' é formato exponencial , 'f' é formato decimal
+	* \param precision: Precisão com a qual os valores numericos serão exibidos
+	*/
 	void setTableData(Matrix values,char mode='e',int precision=6);
+
+	/**
+	* \brief Metodo para inserir valores numericos na tabela a partir da coluna zero e linha zero
+	* \param xml: Objeto EDomElement com o xml contendo os valores numéricos a serem exibidos na tabela
+	* \param mode  : Tipo de exibição com que os valores numéricos aparecerão: 'e' é formato exponencial , 'f' é formato decimal
+	* \param precision: Precisão com a qual os valores numericos serão exibidos
+	*/
 	void setTableData(EDomElement xml,char mode='e',int precision=6);
+
+	/**
+	* \brief Metodo para inserir valores numericos na tabela a partir da coluna zero e linha zero
+	* \param xmlString: String com o xml contendo os valores numéricos a serem exibidos na tabela
+	* \param mode  : Tipo de exibição com que os valores numéricos aparecerão: 'e' é formato exponencial , 'f' é formato decimal
+	* \param precision: Precisão com a qual os valores numericos serão exibidos
+	*/
 	void setTableData(string xmlString,char mode='e',int precision=6);
-	void setVerticalHeadersLabelsHtml(QStringList list);
+
+	/**
+	*  \brief Metodo para redimensionar a tabela de forma que ela fique em um tamanho compacto, tentando sempre não prejudicar a visulaização dos dados
+	*/
 	void resizeTable();
+
+	/**
+	* \brief Metodo que permite habilitar a copia automatica dos conteudos das celulas apenas por selecionad as celulas
+	* \param enable : Habilita o modo automatico de copia
+	*/
 	void enableAutoCopy(bool enable=true);
+
+	/**
+	* \brief Metodo que permite especificar a partir de onde será colocada matriz com os valores numericos nas tabelas
+	* \param input : Matriz que contem os valores numericos a serem colocados na tabela
+	* \param row   : Linha inicial do preenchimento das celulas
+	* \param column: Coluna inicial do preenchimento das celulas
+	* \param mode  : Tipo de exibição com que os valores numéricos aparecerão: 'e' é formato exponencial , 'f' é formato decimal
+	* \param precision: Precisão com a qual os valores numericos serão exibidos
+	* \attention Cuidado ao sobrescrever celulas já preenchidas
+	*/
 	void putIn(Matrix input, int row, int column,char mode='e',int precision=6);
+
+	/**
+	* \brief Metodo para inserir em deter
+	* \param list  : Lista contendo as QStrings que serao colocadas nas celulas
+	* \param column: numerod  da coluna em que sera inserida a lista
+	*/
 	void putInColumn(QStringList list, int column);
-	void putInRow(QStringList list,int row);
 
+	/*void putInRow(QStringList list,int row);
 	//void setColumnType(int colIndex,QString type="QSpinBox", bool enable=true, double minValue=0, double maxValue=100);
-	void setType(int column, QString type="NONE");
-	bool checkTypeds(int column,QString type);
-	QString getType(int column);
+	//void setType(int column, QString type="NONE");
+	//bool checkTypeds(int column,QString type);
+	//QString getType(int column);
 
-	QSpinBox * getItemSpinBox();
-	QDoubleSpinBox* getItemDoubleSpinBox();
-	int getCurrentSpinBoxRow();
-	int getCurrentSpinBoxColumn();
-	int getCurrentDoubleSpinBoxRow();
-	int getCurrentDoubleSpinBoxColumn();
+	//QSpinBox * getItemSpinBox();
+	//QDoubleSpinBox* getItemDoubleSpinBox();
+	//int getCurrentSpinBoxRow();
+	//int getCurrentSpinBoxColumn();
+	//int getCurrentDoubleSpinBoxRow();
+	//int getCurrentDoubleSpinBoxColumn();
+	*/
 
+	/**
+	* \brief Metodo auxiliar para apagar seleção de celulas
+	*/
 	void clearSelection();
-	void setMode(char newMode);
-	void setDecimals(int newDecimals);
-	char getMode();
-	int getDecimals();
-	double getPreviousValue();
-public slots:
-	void autoCopy();
-	void enableAutoCopy(int enable);
-        void updateTableValues(char mode='f', int precision=4);
-        void updateModoValues(int modo=0);
-        void updateDecimalsValues(int decimal=4);
-	void selectionBackground();
-	void avaliateType(QTableWidgetItem *);
-	void avaliateType(int row, int col);
-	void validateItem();
 
+	/**
+	* \brief Metodo para alterar o modo de exibição dos valores numéricos
+	* \param newMode  : Tipo de exibição com que os valores numéricos aparecerão: 'e' é formato exponencial , 'f' é formato decimal
+	*/
+	void setMode(char newMode);
+
+	/**
+	* \brief Metodo para alterar a precisão com que os valores numéricos sao exibidos
+	* \param newDecimals: Precisão com a qual os valores numericos serão exibidos
+	*/
+	void setDecimals(int newDecimals);
+
+	/**
+	* \brief Metodo para retornar o modo atual
+	* \return char : Caracter contendo o modo de exibição: 'e' é formato exponencial , 'f' é formato decimal
+	*/
+	char getMode();
+
+	/**
+	* \brief Metodo para retornar a precisão atual
+	* \return int : Inteiro contendo o numero de casas decimais exibidas
+	*/
+	int getDecimals();
+
+	//double getPreviousValue();
+
+public slots:
+
+	/**
+	* \brief Metodo que coloca na area de transferencia o conteudo da(s) celula(s) selecionada(s)
+	*/
+	void autoCopy();
+
+	/**
+	* \brief Override de metodo adequação do connect com o signal da QCheckBox, a saber, "stateChanged(int)"
+	* \param int : Inteiro com o codigo de conexão com QCheckBox: a saber 0 desconecta e 1 conecta sinal
+	*/
+	void enableAutoCopy(int enable);
+
+	/**
+	* \brief Atualiza os valores da tabela para a formataçao informada nos parametros
+	* \param mode  : Tipo de exibição com que os valores numéricos aparecerão: 'e' é formato exponencial , 'f' é formato decimal
+	* \param precision: Precisão com a qual os valores numericos serão exibidos
+	* \attention Comportamento impreciso se houver valorer nao-numericos
+	*/
+
+	void updateTableValues(char mode='f', int precision=4);
+
+	/**
+	* \brief Atualiza os valores da tabela para a formataçao informada no parametro
+	* \param mode  : Tipo de exibição com que os valores numéricos aparecerão: 0 é formato exponencial , 1 é formato decimal
+	* \attention Comportamento impreciso se houver valorer nao-numericos
+	*/
+	void updateModoValues(int modo=0);
+
+	/**
+	* \brief Atualiza os valoes da tabela para o numero de casa decimais
+	* \param decimal : Numero de casa decimais para os valores numericos exibidos
+	* \attention Comportamento impreciso se houver valorer nao-numericos
+	*/
+	void updateDecimalsValues(int decimal=4);
+
+	//void selectionBackground();
+	//void avaliateType(QTableWidgetItem *);
+	//void avaliateType(int row, int col);
+	//void validateItem();
+
+/*
 signals:
 	void focusReceived();
 	void validatedItem(int, int, int);
 	void validatedItem(int, int, double);
+*/
 
 protected:
 	// void resizeEvent(QResizeEvent *event);
+	/**
+	* \brief Metodo reimplementado do QTableWidget
+	* \param QObject : Objeto que gerou um evento
+	* \param QEvent : Evento gerado pelo objeto
+	*/
 	bool eventFilter(QObject *, QEvent *);
+
+	/**
+	* \brief Metodo reimplementado do QTableWidget. Quando o usuario pressiona ctrl+c, o conteudo das celulas selecionadas e posto na area de transferencia
+	* \param Objeto de evento contendo as informaçoes sobre as teclas pressionadas
+	*/
 	void keyPressEvent(QKeyEvent *event);
+
+	/*
 	void focusInEvent(QFocusEvent *event);
 	void focusOutEvent(QFocusEvent *event);
 	//void dragMoveEvent(QDragMoveEvent *event);
+	*/
 };
 
 } // namespace efoto
--- trunk_candidata/qt/infrastructure/FlightDirectionForm.h
+++ trunk_candidata/qt/infrastructure/FlightDirectionForm.h
@@ -8,18 +8,48 @@ namespace uerj {
 namespace eng {
 namespace efoto {
 
+/**
+* \file FlightDirectionForm.h
+* \class FlightDirectionform
+* \brief Classe que controla o widget usado para indicar direçao de voo
+* \copyright E-foto group
+* \author Paulo Andre Batista Pupim
+*/
 class FlightDirectionForm : public QWidget, public Ui::FlightDirectionForm
 {
 	Q_OBJECT
 public:
+
+	/**
+	* \brief Construtor padrao
+	* \param parent : ponteiro para o QWidget pai
+	*/
 	FlightDirectionForm(QWidget *parent = 0);
 
 public slots:
+
+	/**
+	* brief Metodo que emite um sinal com o valor do angulo, em radianos, quando o botao accept e acionado, e o texto da QComboBox contendo o arquivo da imagem
+	*/
 	void acceptValues();
+
+	/**
+	* \brief Metodo que altera o valor do angulo contido na QSpinBox
+	* \param value : Valor do angulo em graus
+	*/
 	void setSpinValue(int value);
+
+	/**
+	* \brief Metodo que altera o valor do angulo apontado no QDial
+	* \param value : Valor do angulo em graus
+	*/
 	void setDialValue(int value);
 
 signals:
+
+	/**
+	* \brief Sinal contendo o texto com o arquivo de imagem e o valor do angulo da direçao de voo em radianos
+	*/
 	void valuesFlightDirectionForm(QString,double);
 
 };
