180200da1132136fd34362362e46c7ff70995af2
irvingbadolato <irvingbadolato@d8036de5-5264-49cf-ab17-72cb7252746c>
2016-05-18 05:14:41 +0000

    Review of stereoplotter layout. No functional changes.

--- qt/interface/SPUserInterface_Qt.h
+++ qt/interface/SPUserInterface_Qt.h
@@ -33,7 +33,7 @@ class TreeFeatures;
 
 class SPUserInterface_Qt : public QMainWindow, public Ui::StereoPlotterForm, public SPUserInterface
 {
-	Q_OBJECT
+    Q_OBJECT
 
 public:
 
@@ -41,61 +41,61 @@ public slots:
 
 protected:
     static SPUserInterface_Qt* spInst;
-	SPUserInterface_Qt(SPManager* manager, QWidget* parent = 0, Qt::WindowFlags fl = Qt::Window);
-	~SPUserInterface_Qt();
-	SPManager *manager;
-	SeparatedStereoViewer* viewerSeparated;
-	SeparatedStereoToolsBar* toolsSeparated;
-	StereoViewer* viewer;
-	StereoToolsBar* tools;
-        TreeFeatures *tree;
-        void closeEvent(QCloseEvent *e);
-	void updateData();
-	void updateTable();
+    SPUserInterface_Qt(SPManager* manager, QWidget* parent = 0, Qt::WindowFlags fl = Qt::Window);
+    ~SPUserInterface_Qt();
+    SPManager *manager;
+    SeparatedStereoViewer* viewerSeparated;
+    SeparatedStereoToolsBar* toolsSeparated;
+    StereoViewer* viewer;
+    StereoToolsBar* tools;
+    TreeFeatures *tree;
+    void closeEvent(QCloseEvent *e);
+    void updateData();
+    void updateTable();
 
 public slots:
-	virtual void languageChange();
-	void updateClass(int feat_type);
-	void onLoadButton();
-	void onSaveButton();
-        void onSaveTxtButton();
-	void onAddButton();
-	void onRemoveButton();
-	void onRemoveAllButton();
-	void onAddPtButton();
-	void onEditPtButton();
-	void onSelPtButton();
-	void onRemovePtButton();
-        void onFeatureListClicked(QModelIndex index);
-	void onChangePair(int pos);
-	void changePair(int leftKey, int rightKey);
-	void onFeatureSelected();
-	void onCloseFeature();
-	void stereoClicked(QPointF, QPointF);
-	void stereoMoved(QPointF, QPointF);
-        void centerImages(ObjectSpaceCoordinate coord, double zoom);
-        void adjustFit(int width, int height);
-        void setReverseLensGlasses(int option);
-        void setColorMaskLeft(int option);
-        void setColorMaskRight(int option);
-        void onStereoModeChanged(int option);
+    virtual void languageChange();
+    void updateClass(int feat_type);
+    void onLoadButton();
+    void onSaveButton();
+    void onSaveTxtButton();
+    void onAddButton();
+    void onRemoveButton();
+    void onRemoveAllButton();
+    void onAddPtButton();
+    void onEditPtButton();
+    void onSelPtButton();
+    void onRemovePtButton();
+    void onFeatureListClicked(QModelIndex index);
+    void onChangePair(int pos);
+    void changePair(int leftKey, int rightKey);
+    void onFeatureSelected();
+    void onCloseFeature();
+    void stereoClicked(QPointF, QPointF);
+    void stereoMoved(QPointF, QPointF);
+    void centerImages(ObjectSpaceCoordinate coord, double zoom);
+    void adjustFit(int width, int height);
+    void setReverseLensGlasses(int option);
+    void setColorMaskLeft(int option);
+    void setColorMaskRight(int option);
+    void onStereoModeChanged(int option);
 
 private:
-	void init();
-	//void close();
-	bool allow_close;
-	int measure_mode; // 0 - None, 1- Add, 2- Edit, 3- Select
-        // Anagliph colors: Red, Green, Blue, Cyan, Magenta, Yellow
-        bool color_map[6][3];
-        QString lastDir;
+    void init();
+    //void close();
+    bool allow_close;
+    int measure_mode; // 0 - None, 1- Add, 2- Edit, 3- Select
+    // Anagliph colors: Red, Green, Blue, Cyan, Magenta, Yellow
+    bool color_map[6][3];
+    QString lastDir;
 
 public:
-	static SPUserInterface_Qt* instance(SPManager* manager);
-	// Other Methods
-	//
-	bool exec();
+    static SPUserInterface_Qt* instance(SPManager* manager);
+    // Other Methods
+    //
+    bool exec();
     void addImagePair(char *);
-        void showErrorMessage(QString msg);
+    void showErrorMessage(QString msg);
 
 };
 
