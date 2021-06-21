ddc7107de08c232e3a6a86482eaa8d479c0cd271
jaraujouerj <araujo@eng.uerj.br>
2021-06-21 18:02:41 -0300

    dead code deleted from Point.*

--- c/photogrammetry/Point.cpp
+++ c/photogrammetry/Point.cpp
@@ -1,7 +1,7 @@
 /*******************************************************************************
 	  Point.cpp
 *******************************************************************************/
-/*Copyright 2002-2014 e-foto team (UERJ)
+/*Copyright 2002-2021 e-foto team (UERJ)
   This file is part of e-foto.
 
     e-foto is free software: you can redistribute it and/or modify
@@ -27,64 +27,23 @@ namespace uerj {
 namespace eng {
 namespace efoto {
 
-// Constructors
-
-/**
- *
- */
 Point::Point()
 {
-	setId(0);
+    setId(0);
 }
 
-/**
- *
- */
 Point::Point(int myId)
 {
-	setId(myId);
+    setId(myId);
 }
 
-/**
- *
- */
 Point::~Point()
 {
 }
 
-// Private attribute accessor methods
-//
-
-/**
- * Set the value of id
- * @param newId the new value of id
- */
 void Point::setId(int newId)
 {
-	id = newId;
-}
-
-/**
- * Set the value of gcpId
- * @param newId the new value of gcpId
- */
-void Point::setPointId(std::string newPointId)
-{
-	pointId = newPointId;
-}
-
-/**
- * Set the value of description
- * @param newId the new value of description
- */
-void Point::setDescription(std::string newDescription)
-{
-	description = newDescription;
-}
-
-void Point::setType(PointType type)
-{
-	this->type = type;
+    id = newId;
 }
 
 /**
@@ -93,7 +52,7 @@ void Point::setType(PointType type)
  */
 int Point::getId()
 {
-	return id;
+    return id;
 }
 
 /**
@@ -102,7 +61,7 @@ int Point::getId()
  */
 std::string Point::getPointId()
 {
-	return pointId;
+    return pointId;
 }
 
 /**
@@ -111,16 +70,13 @@ std::string Point::getPointId()
  */
 std::string Point::getDescription()
 {
-	return description;
+    return description;
 }
 
 Point::PointType Point::getType()
 {
-	return type;
+    return type;
 }
-
-// Composed Objects accessor methods
-//
 
 /**
  * Set the value of objectCoordinate
@@ -128,7 +84,7 @@ Point::PointType Point::getType()
  */
 void Point::setObjectCoordinate(ObjectSpaceCoordinate newObjectCoordinate)
 {
-	objectCoordinate = newObjectCoordinate;
+    objectCoordinate = newObjectCoordinate;
 }
 
 /**
@@ -137,25 +93,7 @@ void Point::setObjectCoordinate(ObjectSpaceCoordinate newObjectCoordinate)
  */
 ObjectSpaceCoordinate Point::getObjectCoordinate()
 {
-	return objectCoordinate;
-}
-
-/**
- * Set all the values of imageCoordinates deque at once
- * @param newimageCoordinates the new values
- */
-void Point::setImageCoordinates(std::deque<ImageSpaceCoordinate> newimageCoordinates)
-{
-	imageCoordinates = newimageCoordinates;
-}
-
-/**
- * Set all the values of DetectorCoordinates deque at once
- * @param newDetectorCoordinates the new values
- */
-void Point::setDetectorCoordinates(std::deque<DetectorSpaceCoordinate> newDetectorCoordinates)
-{
-	detectorCoordinates = newDetectorCoordinates;
+    return objectCoordinate;
 }
 
 /**
@@ -164,18 +102,8 @@ void Point::setDetectorCoordinates(std::deque<DetectorSpaceCoordinate> newDetect
  */
 std::deque<ImageSpaceCoordinate> Point::getImageCoordinates()
 {
-	return imageCoordinates;
+    return imageCoordinates;
 }
-
-/**
- * Get the value of DetectorCoordinates deque
- * @return the values of DetectorCoordinates deque
- */
-std::deque<DetectorSpaceCoordinate> Point::getDetectorCoordinates()
-{
-	return detectorCoordinates;
-}
-
 
 /**
  * Put one value in imageCoordinates deque
@@ -183,35 +111,29 @@ std::deque<DetectorSpaceCoordinate> Point::getDetectorCoordinates()
  */
 int Point::putImageCoordinate(ImageSpaceCoordinate newimageCoordinate)
 {
-	if (imageCoordinates.empty())
-	{
-		imageCoordinates.push_back(newimageCoordinate);
-	}
-	else
-	{
-		if (imageCoordinates.back().getImageId() < newimageCoordinate.getImageId())
-		{
-			imageCoordinates.push_back(newimageCoordinate);
-		}
-		else
-		{
-			for (unsigned int i = 0; i < imageCoordinates.size(); i++)
-			{
-				if (imageCoordinates.at(i).getImageId() > newimageCoordinate.getImageId())
-				{
-					imageCoordinates.insert(imageCoordinates.begin()+i,newimageCoordinate);
-					break;
-				}
-				else if (imageCoordinates.at(i).getImageId() == newimageCoordinate.getImageId())
-				{
-					imageCoordinates.erase(imageCoordinates.begin()+i);
-					imageCoordinates.insert(imageCoordinates.begin()+i,newimageCoordinate);
-					return 1;
-				}
-			}
-		}
-	}
-	return 0;
+    if (imageCoordinates.empty()) {
+        imageCoordinates.push_back(newimageCoordinate);
+    }
+    else {
+        if (imageCoordinates.back().getImageId() < newimageCoordinate.getImageId()) {
+            imageCoordinates.push_back(newimageCoordinate);
+        }
+        else {
+            for (unsigned int i = 0; i < imageCoordinates.size(); i++) {
+                if (imageCoordinates.at(i).getImageId() > newimageCoordinate.getImageId()) {
+                    imageCoordinates.insert(imageCoordinates.begin() + i, newimageCoordinate);
+                    break;
+                }
+                else if (imageCoordinates.at(i).getImageId() == newimageCoordinate.getImageId()) {
+                    imageCoordinates.erase(imageCoordinates.begin() + i);
+                    imageCoordinates.insert(imageCoordinates.begin() + i, newimageCoordinate);
+                    return 1;
+                }
+            }
+        }
+    }
+
+    return 0;
 }
 
 /**
@@ -220,12 +142,14 @@ int Point::putImageCoordinate(ImageSpaceCoordinate newimageCoordinate)
  */
 int Point::putDetectorCoordinate(DetectorSpaceCoordinate newDetectorCoordinate)
 {
+    int imageId = newDetectorCoordinate.getImageId();
 
-	int imageId = newDetectorCoordinate.getImageId();
-	if (hasDetectorCoordinate(imageId))
-		return 1;
-	detectorCoordinates.push_back(newDetectorCoordinate);
-	return 0;
+    if (hasDetectorCoordinate(imageId)) {
+        return 1;
+    }
+
+    detectorCoordinates.push_back(newDetectorCoordinate);
+    return 0;
 }
 
 /**
@@ -235,11 +159,12 @@ int Point::putDetectorCoordinate(DetectorSpaceCoordinate newDetectorCoordinate)
  */
 ImageSpaceCoordinate Point::getImageCoordinate(int imageId)
 {
+    for (unsigned int i = 0; i < imageCoordinates.size(); i++)
+        if (imageCoordinates.at(i).getImageId() == imageId) {
+            return imageCoordinates.at(i);
+        }
 
-	for (unsigned int i = 0; i < imageCoordinates.size(); i++)
-		if (imageCoordinates.at(i).getImageId() == imageId)
-			return imageCoordinates.at(i);
-	return ImageSpaceCoordinate();
+    return ImageSpaceCoordinate();
 }
 
 /**
@@ -249,63 +174,21 @@ ImageSpaceCoordinate Point::getImageCoordinate(int imageId)
  */
 DetectorSpaceCoordinate Point::getDetectorCoordinate(int imageId)
 {
+    for (unsigned int i = 0; i < detectorCoordinates.size(); i++)
+        if (detectorCoordinates.at(i).getImageId() == imageId) {
+            return detectorCoordinates.at(i);
+        }
 
-	for (unsigned int i = 0; i < detectorCoordinates.size(); i++)
-		if (detectorCoordinates.at(i).getImageId() == imageId)
-			return detectorCoordinates.at(i);
-	return DetectorSpaceCoordinate();
+    return DetectorSpaceCoordinate();
 }
 
-/**
- *
- */
 ImageSpaceCoordinate Point::getImageCoordinateAt(unsigned int index)
 {
-	if (index < imageCoordinates.size())
-		return imageCoordinates.at(index);
-	return ImageSpaceCoordinate();
-}
+    if (index < imageCoordinates.size()) {
+        return imageCoordinates.at(index);
+    }
 
-/**
- *
- */
-DetectorSpaceCoordinate Point::getDetectorCoordinateAt(unsigned int index)
-{
-	if (index < detectorCoordinates.size())
-		return detectorCoordinates.at(index);
-	return DetectorSpaceCoordinate();
-}
-
-/**
- *
- */
-unsigned int Point::countImageCoordinates()
-{
-	return imageCoordinates.size();
-}
-
-/**
- *
- */
-unsigned int Point::countDetectorCoordinates()
-{
-	return detectorCoordinates.size();
-}
-
-/**
- * Clear all the values of imageCoordinates deque
- */
-void Point::clearImageCoordinates()
-{
-	imageCoordinates.clear();
-}
-
-/**
- * Clear all the values of DetectorCoordinates deque
- */
-void Point::clearDetectorCoordinates()
-{
-	detectorCoordinates.clear();
+    return ImageSpaceCoordinate();
 }
 
 /**
@@ -314,48 +197,27 @@ void Point::clearDetectorCoordinates()
  */
 void Point::deleteImageCoordinate(int imageId)
 {
-
-	for (unsigned int i = 0; i < imageCoordinates.size(); i++)
-		if (imageCoordinates.at(i).getImageId() == imageId)
-		{
-			imageCoordinates.erase(imageCoordinates.begin()+i);
-			break;
-		}
+    for (unsigned int i = 0; i < imageCoordinates.size(); i++)
+        if (imageCoordinates.at(i).getImageId() == imageId) {
+            imageCoordinates.erase(imageCoordinates.begin() + i);
+            break;
+        }
 }
 
-/**
- * Delete the value of DetectorCoordinate with a specific imageId
- * @param imageId the specified imageId
- */
-void Point::deleteDetectorCoordinate(int imageId)
-{
-
-	for (unsigned int i = 0; i < detectorCoordinates.size(); i++)
-		if (detectorCoordinates.at(i).getImageId() == imageId)
-		{
-			detectorCoordinates.erase(detectorCoordinates.begin()+i);
-			break;
-		}
-}
-
-
-
-// Associated object accessor methods
-//
-
-/**
- *
- */
 void Point::putImage(Image* newImageAssociation)
 {
-	bool insert = true;
-	// Eliminamos primeiro a possibilidade duplicar uma associação.
-	for (unsigned int i = 0; i < myImages.size(); i++)
-		if (myImages.at(i) == newImageAssociation)
-			insert = false;
-	// Fazemos a nova associação.
-	if (insert)
-		myImages.push_back(newImageAssociation);
+    bool insert = true;
+
+    // Eliminamos primeiro a possibilidade duplicar uma associação.
+    for (unsigned int i = 0; i < myImages.size(); i++)
+        if (myImages.at(i) == newImageAssociation) {
+            insert = false;
+        }
+
+    // Fazemos a nova associação.
+    if (insert) {
+        myImages.push_back(newImageAssociation);
+    }
 }
 
 void Point::clearImages()
@@ -365,44 +227,25 @@ void Point::clearImages()
 
 void Point::removeImage(int id)
 {
-    for (int i = myImages.size()-1; i >= 0; i--)
-        if (myImages.at(i)->getId() == id)
-            myImages.erase(myImages.begin()+i);
+    for (int i = myImages.size() - 1; i >= 0; i--)
+        if (myImages.at(i)->getId() == id) {
+            myImages.erase(myImages.begin() + i);
+        }
 }
 
-/**
- *
- */
-Image* Point::getImage(int imageId)
-{
-	for (unsigned int i = 0; i < myImages.size(); i++)
-		if (myImages.at(i)->getId() == imageId)
-			return myImages.at(i);
-	return NULL;
-}
-
-/**
- *
- */
 int Point::countImages()
 {
-	return myImages.size();
+    return myImages.size();
 }
 
-/**
- *
- */
 Image* Point::getImageAt(unsigned int index)
 {
-	if (index < myImages.size())
-		return myImages.at(index);
-	return NULL;
+    if (index < myImages.size()) {
+        return myImages.at(index);
+    }
+
+    return NULL;
 }
-
-
-
-// Cheking methods
-//
 
 /**
  * Check if imageCoordinates has a member with a specific imageId
@@ -410,11 +253,12 @@ Image* Point::getImageAt(unsigned int index)
  */
 bool Point::hasImageCoordinate(int imageId)
 {
+    for (unsigned int i = 0; i < imageCoordinates.size(); i++)
+        if (imageCoordinates.at(i).getImageId() == imageId) {
+            return true;
+        }
 
-	for (unsigned int i = 0; i < imageCoordinates.size(); i++)
-		if (imageCoordinates.at(i).getImageId() == imageId)
-			return true;
-	return false;
+    return false;
 }
 
 /**
@@ -423,121 +267,107 @@ bool Point::hasImageCoordinate(int imageId)
  */
 bool Point::hasDetectorCoordinate(int imageId)
 {
+    for (unsigned int i = 0; i < detectorCoordinates.size(); i++)
+        if (detectorCoordinates.at(i).getImageId() == imageId) {
+            return true;
+        }
 
-	for (unsigned int i = 0; i < detectorCoordinates.size(); i++)
-		if (detectorCoordinates.at(i).getImageId() == imageId)
-			return true;
-	return false;
-
+    return false;
 }
 
-// EObject methods
-//
-
-/**
- *
- */
 std::string Point::objectType(void)
 {
     std::stringstream result;
-	result << "Point " << id;
-	return result.str();
+    result << "Point " << id;
+    return result.str();
 }
 
-/**
- *
- */
 std::string Point::objectAssociations(void)
 {
     std::stringstream result;
-	if (countImages() != 0)
-		result << "Image";
-	for (int i = 0; i < countImages(); i++)
-		result << " " << getImageAt(i)->getId();
-	return result.str();
+
+    if (countImages() != 0) {
+        result << "Image";
+    }
+
+    for (int i = 0; i < countImages(); i++) {
+        result << " " << getImageAt(i)->getId();
+    }
+
+    return result.str();
 }
 
-/**
- *
- */
 bool Point::is(std::string s)
 {
-	return (s == "Point" ? true : false);
+    return (s == "Point" ? true : false);
 }
 
 void Point::xmlSetData(std::string xml)
 {
-	EDomElement root(xml);
-	id = Conversion::stringToInt(root.attribute("key"));
-	type = readPointType(root.attribute("type"));
-	pointId = root.elementByTagName("pointId").toString();
-	description = root.elementByTagName("description").toString();
-	objectCoordinate.xmlSetData(root.elementByTagName("spatialCoordinates").getContent());
+    EDomElement root(xml);
+    id = Conversion::stringToInt(root.attribute("key"));
+    type = readPointType(root.attribute("type"));
+    pointId = root.elementByTagName("pointId").toString();
+    description = root.elementByTagName("description").toString();
+    objectCoordinate.xmlSetData(root.elementByTagName("spatialCoordinates").getContent());
     std::deque<EDomElement> xmlimageCoordinates = root.elementsByTagName("imageCoordinates");
-	imageCoordinates.clear();
-	for (unsigned int i = 0; i < xmlimageCoordinates.size(); i++)
-	{
-		ImageSpaceCoordinate* disc = new ImageSpaceCoordinate;
-		disc->xmlSetData(xmlimageCoordinates.at(i).getContent());
-		imageCoordinates.push_back(*disc);
-	}
+    imageCoordinates.clear();
+
+    for (unsigned int i = 0; i < xmlimageCoordinates.size(); i++) {
+        ImageSpaceCoordinate* disc = new ImageSpaceCoordinate;
+        disc->xmlSetData(xmlimageCoordinates.at(i).getContent());
+        imageCoordinates.push_back(*disc);
+    }
 }
 
 std::string Point::xmlGetData()
 {
     std::stringstream result;
-	result << "<point key=\"" << Conversion::intToString(id) << "\" type=\"" << writePointType(type) << "\">\n";
-	result << "<pointId>" << pointId << "</pointId>\n";
-	result << "<description>" << description << "</description>\n";
-	result << objectCoordinate.xmlGetData();
-	result << "<imagesMeasurements>\n";
-	for (unsigned int i = 0; i < imageCoordinates.size(); i++)
-	{
-		result << imageCoordinates.at(i).xmlGetData();
-	}
-	result << "</imagesMeasurements>\n";
-	result << "</point>\n";
-	return result.str();
+    result << "<point key=\"" << Conversion::intToString(id) << "\" type=\"" << writePointType(type) << "\">\n";
+    result << "<pointId>" << pointId << "</pointId>\n";
+    result << "<description>" << description << "</description>\n";
+    result << objectCoordinate.xmlGetData();
+    result << "<imagesMeasurements>\n";
+
+    for (unsigned int i = 0; i < imageCoordinates.size(); i++) {
+        result << imageCoordinates.at(i).xmlGetData();
+    }
+
+    result << "</imagesMeasurements>\n";
+    result << "</point>\n";
+    return result.str();
 }
 
 Point::PointType Point::readPointType(std::string type)
 {
-	if (type == "control")
-	{
-		return CONTROL;
-	}
-	else if (type == "checking")
-	{
-		return CHECKING;
-	}
-	else if (type == "photogrammetric")
-	{
-		return PHOTOGRAMMETRIC;
-	}
-	else
-	{
-		return UNKNOWN;
-	}
+    if (type == "control") {
+        return CONTROL;
+    }
+    else if (type == "checking") {
+        return CHECKING;
+    }
+    else if (type == "photogrammetric") {
+        return PHOTOGRAMMETRIC;
+    }
+    else {
+        return UNKNOWN;
+    }
 }
 
 std::string Point::writePointType(PointType type)
 {
-	if (type == CONTROL)
-	{
-		return "control";
-	}
-	else if (type == CHECKING)
-	{
-		return "checking";
-	}
-	else if (type == PHOTOGRAMMETRIC)
-	{
-		return "photogrammetric";
-	}
-	else
-	{
-		return "unknown";
-	}
+    if (type == CONTROL) {
+        return "control";
+    }
+    else if (type == CHECKING) {
+        return "checking";
+    }
+    else if (type == PHOTOGRAMMETRIC) {
+        return "photogrammetric";
+    }
+    else {
+        return "unknown";
+    }
 }
 
 } // namespace efoto
--- c/photogrammetry/Point.h
+++ c/photogrammetry/Point.h
@@ -1,6 +1,6 @@
 #ifndef POINT_H
 #define POINT_H
-/*Copyright 2002-2014 e-foto team (UERJ)
+/*Copyright 2002-2021 e-foto team (UERJ)
   This file is part of e-foto.
 
     e-foto is free software: you can redistribute it and/or modify
@@ -38,92 +38,54 @@ class Image;
 * \copyright E-Foto group
 * \authors Rafael Aguiar & Irving Badolato
 */
-class Point : public EObject
-{
+class Point : public EObject {
 public:
 
-	enum PointType { PHOTOGRAMMETRIC, CHECKING, CONTROL, UNKNOWN};
+    enum PointType { PHOTOGRAMMETRIC, CHECKING, CONTROL, UNKNOWN};
 
 protected:
-	int id;
+    int id;
     std::string pointId;
     std::string description;
-	PointType type;
-
-	ObjectSpaceCoordinate objectCoordinate;
+    PointType type;
+    ObjectSpaceCoordinate objectCoordinate;
     std::deque<ImageSpaceCoordinate> imageCoordinates;
     std::deque<DetectorSpaceCoordinate> detectorCoordinates;
-
     std::deque<Image*> myImages;
-
     PointType readPointType(std::string type);
     std::string writePointType(PointType type);
 
 public:
-
-	// Constructors
-	//
-	Point();
+    Point();
     explicit Point(int myId);
-	virtual ~Point();
+    virtual ~Point();
 
-	// Private attribute accessor methods
-	//
-	void setId(int newId);
-    void setPointId(std::string newPointId);
-    void setDescription(std::string newDescription);
-	void setType(PointType type);
-	int getId();
+    void setId(int newId);
+    int getId();
     std::string getPointId();
     std::string getDescription();
-	PointType getType();
-
-	// Composed Objects accessor methods
-	//
-	void setObjectCoordinate(ObjectSpaceCoordinate newObject);
-	ObjectSpaceCoordinate getObjectCoordinate();
-
-    void setImageCoordinates(std::deque<ImageSpaceCoordinate> newImageCoordinates);
-    void setDetectorCoordinates(std::deque<DetectorSpaceCoordinate> newDetectorCoordinates);
+    PointType getType();
+    void setObjectCoordinate(ObjectSpaceCoordinate newObject);
+    ObjectSpaceCoordinate getObjectCoordinate();
     std::deque<ImageSpaceCoordinate> getImageCoordinates();
-    std::deque<DetectorSpaceCoordinate> getDetectorCoordinates();
-
-	int putImageCoordinate(ImageSpaceCoordinate newimageCoordinate);
-	int putDetectorCoordinate(DetectorSpaceCoordinate newdetectorCoordinate);
-	ImageSpaceCoordinate getImageCoordinate(int imageId);
-	DetectorSpaceCoordinate getDetectorCoordinate(int imageId);
-	ImageSpaceCoordinate getImageCoordinateAt(unsigned int index);
-	DetectorSpaceCoordinate getDetectorCoordinateAt(unsigned int index);
-	unsigned int countImageCoordinates();
-	unsigned int countDetectorCoordinates();
-
-	void clearImageCoordinates();
-	void clearDetectorCoordinates();
-	void deleteImageCoordinate(int imageId);
-	void deleteDetectorCoordinate(int imageId);
-
-	// Associated object accessor methods
-	//
-	void putImage(Image* newImageAssociation); // Set point associations. Generally from zero to many images.
-    void clearImages(); // Remove associations.
-    void removeImage(int id); // Remove one association;
-	Image* getImage(int imageId); // Get associated point at image by id.
-	int countImages(); // Inform the number of points associated the image.
+    int putImageCoordinate(ImageSpaceCoordinate newimageCoordinate);
+    int putDetectorCoordinate(DetectorSpaceCoordinate newdetectorCoordinate);
+    ImageSpaceCoordinate getImageCoordinate(int imageId);
+    DetectorSpaceCoordinate getDetectorCoordinate(int imageId);
+    ImageSpaceCoordinate getImageCoordinateAt(unsigned int index);
+    void deleteImageCoordinate(int imageId);
+    void putImage(Image* newImageAssociation);
+    void clearImages();
+    void removeImage(int id);
+    int countImages(); // Inform the number of points associated the image.
     Image* getImageAt(unsigned int index); // Get associated point at image by index in std::deque for iterations.
 
-	// Check methods
-	//
-	bool hasImageCoordinate(int imageId);
-	bool hasDetectorCoordinate(int imageId);
-
-	// EObject methods
-	//
+    bool hasImageCoordinate(int imageId);
+    bool hasDetectorCoordinate(int imageId);
     std::string objectType(void);
     std::string objectAssociations(void);
     bool is(std::string s);
 
-	// Other methods
-	//
     void xmlSetData(std::string xml);
     std::string xmlGetData();
 };
