b1c9636b740a7e6cd5a8afecc6aa4ddb22a777d6
jaraujouerj <araujo@eng.uerj.br>
2021-06-22 00:10:02 -0300

    dead code deleted from DetectorFiducialMark and DetectorSpaceCoordinate

--- c/photogrammetry/DetectorFiducialMark.h
+++ c/photogrammetry/DetectorFiducialMark.h
@@ -1,6 +1,6 @@
 #ifndef DETECTORFIDUCIALMARK_H
 #define DETECTORFIDUCIALMARK_H
-/*Copyright 2002-2014 e-foto team (UERJ)
+/*Copyright 2002-2021 e-foto team (UERJ)
   This file is part of e-foto.
 
     e-foto is free software: you can redistribute it and/or modify
@@ -31,117 +31,99 @@ namespace efoto {
 * \copyright E-Foto group
 * \authors Rafael Aguiar & Irving Badolato
 */
-class DetectorFiducialMark : public AbstractDetectorCoordinate
-{
+class DetectorFiducialMark : public AbstractDetectorCoordinate {
 
 private:
-	int id;
-	int sensorId;
+    int id;
+    int sensorId;
 
 
 public:
 
-	/**
- * \brief Construtor vazio.
- */
-	DetectorFiducialMark();
+    /**
+    * \brief Construtor vazio.
+    */
+    DetectorFiducialMark();
 
-	/**
- * \brief Construtor que cria uma marca fiducial analógica com seus identificadores.
- * \param myId	Identificador da marca fiducial analógica.
- * \param mySensorId	Identificador do sensor ao qual a marca pertence.
- */
+    /**
+    * \brief Construtor que cria uma marca fiducial analógica com seus identificadores.
+    * \param myId	Identificador da marca fiducial analógica.
+    * \param mySensorId	Identificador do sensor ao qual a marca pertence.
+    */
     explicit DetectorFiducialMark(int myId, int mySensorId);
 
-	/**
- * \brief Construtor que cria uma marca fiducial analógica com todos os seus valores.
- * \param myId	Identificador da marca fiducial analógica.
- * \param mySensorId	Identificador do sensor ao qual a marca pertence.
- * \param myUnit	Texto descritor da unidade de medidas considerada para a marca.
- * \param myXi	Valor Xi.
- * \param myEta	Valor Eta.
- * \param mySigmaXi	Valor do erro na direção Xi.
- * \param mySigmaEta	Valor do erro na direção Eta.
- * \param mySigmaXiEta	Valor de erro conjunto para as direções Xi e Eta.
- */
+    /**
+    * \brief Construtor que cria uma marca fiducial analógica com todos os seus valores.
+    * \param myId	Identificador da marca fiducial analógica.
+    * \param mySensorId	Identificador do sensor ao qual a marca pertence.
+    * \param myUnit	Texto descritor da unidade de medidas considerada para a marca.
+    * \param myXi	Valor Xi.
+    * \param myEta	Valor Eta.
+    * \param mySigmaXi	Valor do erro na direção Xi.
+    * \param mySigmaEta	Valor do erro na direção Eta.
+    * \param mySigmaXiEta	Valor de erro conjunto para as direções Xi e Eta.
+    */
     DetectorFiducialMark(int myId, int mySensorId, std::string myUnit, double myXi, double myEta, double mySigmaXi = 1, double mySigmaEta = 1, double mySigmaXiEta = 0);
 
-	/**
- * \brief Construtor que cria uma marca fiducial analógica sem erro conhecido usando matrizes.
- * \param myId	Identificador da marca fiducial analógica.
- * \param mySensorId	Identificador do sensor ao qual a marca pertence.
- * \param myPosition	Matriz coluna com os valores de posicionamento da marca.
- */
-	DetectorFiducialMark(int myId, int mySensorId, const PositionMatrix& myPosition);
+    /**
+    * \brief Construtor que cria uma marca fiducial analógica sem erro conhecido usando matrizes.
+    * \param myId	Identificador da marca fiducial analógica.
+    * \param mySensorId	Identificador do sensor ao qual a marca pertence.
+    * \param myPosition	Matriz coluna com os valores de posicionamento da marca.
+    */
+    DetectorFiducialMark(int myId, int mySensorId, const PositionMatrix& myPosition);
 
-	/**
- * \brief Construtor que cria uma marca fiducial analógica com todos os seus valores usando matrizes.
- * \param myId	Identificador da marca fiducial analógica.
- * \param mySensorId	Identificador do sensor ao qual a marca pertence.
- * \param myPositio	Matriz coluna com os valores de posicionamento da marca.
- * \param myPositionSigmas	Matriz sigma com os valores de erro para o posicionamento da marca.
- */
-	DetectorFiducialMark(int myId, int mySensorId, const PositionMatrix& myPosition, const Matrix& myPositionSigmas);
+    /**
+    * \brief Construtor que cria uma marca fiducial analógica com todos os seus valores usando matrizes.
+    * \param myId	Identificador da marca fiducial analógica.
+    * \param mySensorId	Identificador do sensor ao qual a marca pertence.
+    * \param myPositio	Matriz coluna com os valores de posicionamento da marca.
+    * \param myPositionSigmas	Matriz sigma com os valores de erro para o posicionamento da marca.
+    */
+    DetectorFiducialMark(int myId, int mySensorId, const PositionMatrix& myPosition, const Matrix& myPositionSigmas);
 
-	/**
- * \brief Destrutor padrão.
- */
-	~DetectorFiducialMark();
+    /**
+    * \brief Destrutor padrão.
+    */
+    ~DetectorFiducialMark();
 
-	/**
- * \brief Método de acesso que seta o valor de id da marca fiducial analógica.
- * \param newId	Novo identificador da marca fiducial analógica.
- */
-	void setId(int newId);
 
-	/**
- * \brief Método de acesso que seta o valor de id do sensor ao qual a marca pertence.
- * \param newSensorId	Novo identificador do sensor ao qual a marca pertence.
- */
-	void setSensorId(int newSensorId);
+    /**
+    * \brief Método de acesso que retorna o valor de id da marca fiducial analógica.
+    * \return int	Identificador da marca fiducial analógica.
+    */
+    int getId();
 
-	/**
- * \brief Método de acesso que retorna o valor de id da marca fiducial analógica.
- * \return int	Identificador da marca fiducial analógica.
- */
-	int getId();
-
-	/**
- * \brief Método de acesso que retorna o valor de id do sensor ao qual a marca pertence.
- * \return int	Identificador do sensor ao qual a marca pertence.
- */
-	int getSensorId();
-
-	/**
- * \brief Método para emitir o nome de classe.
- * \return std::string	Retorna o nome de classe do objeto.
- */
+    /**
+    * \brief Método para emitir o nome de classe.
+    * \return std::string	Retorna o nome de classe do objeto.
+    */
     std::string objectType(void);
 
-	/**
- * \brief Método para emitir as associações de uma instância.
- * \return std::string	Retorna vazio para esta classe.
- * \deprecated Este método não possui uso ou deve ser evitado o seu uso, pois ele será removido em versões futuras.
- */
+    /**
+    * \brief Método para emitir as associações de uma instância.
+    * \return std::string	Retorna vazio para esta classe.
+    * \deprecated Este método não possui uso ou deve ser evitado o seu uso, pois ele será removido em versões futuras.
+    */
     std::string objectAssociations(void);
 
-	/**
- * \brief Método de teste para o nome/tipo de instância.
- * \param s	Texto com o nome da classe que é esperado.
- * \return bool	Retorna verdadeiro caso o nome passado seja DetectorFiducialMark. Retorna falso no caso contrário.
- */
+    /**
+    * \brief Método de teste para o nome/tipo de instância.
+    * \param s	Texto com o nome da classe que é esperado.
+    * \return bool	Retorna verdadeiro caso o nome passado seja DetectorFiducialMark. Retorna falso no caso contrário.
+    */
     bool is(std::string s);
 
-	/**
- * \brief Método para setar os valores de atributos de uma instância utilizando sua descrição em xml.
- * \param xml	String contendo o xml com todos os valores de atributos adequados a uma instância da classe DetectorFiducialMark.
- */
+    /**
+    * \brief Método para setar os valores de atributos de uma instância utilizando sua descrição em xml.
+    * \param xml	String contendo o xml com todos os valores de atributos adequados a uma instância da classe DetectorFiducialMark.
+    */
     void xmlSetData(std::string xml);
 
-	/**
- * \brief Método para extrair o equivalente em dados xml de uma instância.
- * \return std::string	Retorna o string contendo o xml para uma instância da classe DetectorFiducialMark.
- */
+    /**
+    * \brief Método para extrair o equivalente em dados xml de uma instância.
+    * \return std::string	Retorna o string contendo o xml para uma instância da classe DetectorFiducialMark.
+    */
     std::string xmlGetData();
 
 };
--- c/photogrammetry/DetectorSpaceCoordinate.h
+++ c/photogrammetry/DetectorSpaceCoordinate.h
@@ -1,6 +1,6 @@
 #ifndef DETECTORSPACECOORDINATE_H
 #define DETECTORSPACECOORDINATE_H
-/*Copyright 2002-2014 e-foto team (UERJ)
+/*Copyright 2002-2021 e-foto team (UERJ)
   This file is part of e-foto.
 
     e-foto is free software: you can redistribute it and/or modify
@@ -33,99 +33,92 @@ namespace efoto {
 * \copyright E-Foto group
 * \authors Rafael Aguiar & Irving Badolato
 */
-class DetectorSpaceCoordinate : public AbstractDetectorCoordinate
-{
+class DetectorSpaceCoordinate : public AbstractDetectorCoordinate {
 
 private:
-	int imageId;
+    int imageId;
 
 public:
 
-	/**
- * \brief Construtor vazio.
- */
-	DetectorSpaceCoordinate();
+    /**
+    * \brief Construtor vazio.
+    */
+    DetectorSpaceCoordinate();
 
-	/**
- * \brief Construtor que cria uma coordenada analógica identificada sobre o espaço imagem.
- * \param myImageId	Identificador da imagem à qual a coordenada pertence.
- */
+    /**
+    * \brief Construtor que cria uma coordenada analógica identificada sobre o espaço imagem.
+    * \param myImageId	Identificador da imagem à qual a coordenada pertence.
+    */
     explicit DetectorSpaceCoordinate(int myImageId);
 
-	/**
- * \brief Construtor que cria uma coordenada analógica identificada sobre o espaço imagem com todos os seus valores.
- * \param myImageId	Identificador da imagem à qual a coordenada pertence.
- * \param myUnit	Texto descritor da unidade de medidas considerada para a coordenada.
- * \param myXi	Valor Xi.
- * \param myEta	Valor Eta.
- * \param mySigmaXi	Valor do erro na direção Xi.
- * \param mySigmaEta	Valor do erro na direção Eta.
- * \param mySigmaXiEta	Valor de erro conjunto para as direções Xi e Eta.
- */
+    /**
+    * \brief Construtor que cria uma coordenada analógica identificada sobre o espaço imagem com todos os seus valores.
+    * \param myImageId	Identificador da imagem à qual a coordenada pertence.
+    * \param myUnit	Texto descritor da unidade de medidas considerada para a coordenada.
+    * \param myXi	Valor Xi.
+    * \param myEta	Valor Eta.
+    * \param mySigmaXi	Valor do erro na direção Xi.
+    * \param mySigmaEta	Valor do erro na direção Eta.
+    * \param mySigmaXiEta	Valor de erro conjunto para as direções Xi e Eta.
+    */
     DetectorSpaceCoordinate(int myImageId, std::string myUnit, double myXi, double myEta, double mySigmaXi = 1, double mySigmaEta = 1, double mySigmaXiEta = 0);
 
-	/**
- * \brief Construtor que cria uma coordenada analógica identificada sobre o espaço imagem sem erro conhecido usando matrizes.
- * \param myImageId	Identificador da imagem à qual a coordenada pertence.
- * \param myPositio	Matriz coluna com os valores de posicionamento da coordenada.
- */
-	DetectorSpaceCoordinate(int myImageId, const PositionMatrix& myPosition);
+    /**
+    * \brief Construtor que cria uma coordenada analógica identificada sobre o espaço imagem sem erro conhecido usando matrizes.
+    * \param myImageId	Identificador da imagem à qual a coordenada pertence.
+    * \param myPositio	Matriz coluna com os valores de posicionamento da coordenada.
+    */
+    DetectorSpaceCoordinate(int myImageId, const PositionMatrix& myPosition);
 
-	/**
- * \brief Construtor que cria uma coordenada analógica identificada sobre o espaço imagem com todos os seus valores usando matrizes.
- * \param myImageId	Identificador da imagem à qual a coordenada pertence.
- * \param myPositio	Matriz coluna com os valores de posicionamento da coordenada.
- * \param myPositionSigmas	Matriz sigma com os valores de erro para o posicionamento da coordenada.
- */
-	DetectorSpaceCoordinate(int myImageId, const PositionMatrix& myPosition, const Matrix& myPositionSigmas);
+    /**
+    * \brief Construtor que cria uma coordenada analógica identificada sobre o espaço imagem com todos os seus valores usando matrizes.
+    * \param myImageId	Identificador da imagem à qual a coordenada pertence.
+    * \param myPositio	Matriz coluna com os valores de posicionamento da coordenada.
+    * \param myPositionSigmas	Matriz sigma com os valores de erro para o posicionamento da coordenada.
+    */
+    DetectorSpaceCoordinate(int myImageId, const PositionMatrix& myPosition, const Matrix& myPositionSigmas);
 
-	/**
- * \brief Destrutor padrão.
- */
-	~DetectorSpaceCoordinate();
+    /**
+    * \brief Destrutor padrão.
+    */
+    ~DetectorSpaceCoordinate();
 
-	/**
- * \brief Método de acesso que seta o valor de id da imagem à qual a coordenada pertence.
- * \param newSensorId	Novo identificador da imagem à qual a coordenada pertence.
- */
-	void setImageId(int newImageId);
+    /**
+    * \brief Método de acesso que retorna o valor de id da imagem à qual a coordenada pertence.
+    * \return int	int	Identificador da imagem à qual a coordenada pertence.
+    */
+    int getImageId();
 
-	/**
- * \brief Método de acesso que retorna o valor de id da imagem à qual a coordenada pertence.
- * \return int	int	Identificador da imagem à qual a coordenada pertence.
- */
-	int getImageId();
-
-	/**
- * \brief Método para emitir o nome de classe.
- * \return std::string	Retorna o nome de classe do objeto.
- */
+    /**
+    * \brief Método para emitir o nome de classe.
+    * \return std::string	Retorna o nome de classe do objeto.
+    */
     std::string objectType(void);
 
-	/**
- * \brief Método para emitir as associações de uma instância.
- * \return std::string	Retorna vazio para esta classe.
- * \deprecated Este método não possui uso ou deve ser evitado o seu uso, pois ele será removido em versões futuras.
- */
+    /**
+    * \brief Método para emitir as associações de uma instância.
+    * \return std::string	Retorna vazio para esta classe.
+    * \deprecated Este método não possui uso ou deve ser evitado o seu uso, pois ele será removido em versões futuras.
+    */
     std::string objectAssociations(void);
 
-	/**
- * \brief Método de teste para o nome/tipo de instância.
- * \param s	Texto com o nome da classe que é esperado.
- * \return bool	Retorna verdadeiro caso o nome passado seja DetectorSpaceCoordinate. Retorna falso no caso contrário.
- */
+    /**
+    * \brief Método de teste para o nome/tipo de instância.
+    * \param s	Texto com o nome da classe que é esperado.
+    * \return bool	Retorna verdadeiro caso o nome passado seja DetectorSpaceCoordinate. Retorna falso no caso contrário.
+    */
     bool is(std::string s);
 
-	/**
- * \brief Método para setar os valores de atributos de uma instância utilizando sua descrição em xml.
- * \param xml	std::string contendo o xml com todos os valores de atributos adequados a uma instância da classe DetectorSpaceCoordinate.
- */
+    /**
+    * \brief Método para setar os valores de atributos de uma instância utilizando sua descrição em xml.
+    * \param xml	std::string contendo o xml com todos os valores de atributos adequados a uma instância da classe DetectorSpaceCoordinate.
+    */
     void xmlSetData(std::string xml);
 
-	/**
- * \brief Método para extrair o equivalente em dados xml de uma instância.
- * \return std::string	Retorna o string contendo o xml para uma instância da classe DetectorSpaceCoordinate.
- */
+    /**
+    * \brief Método para extrair o equivalente em dados xml de uma instância.
+    * \return std::string	Retorna o string contendo o xml para uma instância da classe DetectorSpaceCoordinate.
+    */
     std::string xmlGetData();
 
 };
