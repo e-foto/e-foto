d8f7610578170c00a9d317d6fc4f55df498bb3bd
jaraujouerj <araujo@eng.uerj.br>
2021-06-22 19:53:39 -0300

    removing redundant definition and reshow the common definitions file in the qmake project by @irving

--- c/infrastructure/CommonDef.h
+++ c/infrastructure/CommonDef.h
@@ -1,16 +1,11 @@
 #ifndef COMMONDEF_H
 #define COMMONDEF_H
 
-#ifdef WIN64
-#define _USE_MATH_DEFINES // for C++
-#include <cmath>
+#ifdef WIN32
+#define _USE_MATH_DEFINES // for cmath M_PI availability
 #endif
-#ifdef unix
 #include <cmath>
-#endif
 
-typedef enum { CORREGOALEGRE=0, SIRGAS2000, SAD69, WGS84} DatumType;
-
-#define M_PI 3.14159265359
+typedef enum { CORREGOALEGRE = 0, SIRGAS2000, SAD69, WGS84} DatumType;
 
 #endif // COMMONDEF_H
